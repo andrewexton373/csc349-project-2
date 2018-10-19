8928115e8235b02bd33cc7f3fbd1764018f3a732
Andrew Exton <aexton@calpoly.edu>
2018-10-18 18:25:16 -0700

    cleanup

--- part2/MatrixProduct.java
+++ part2/MatrixProduct.java
@@ -1,3 +1,4 @@
+
 /*
     Andrew Exton - aexton
     Jett Moy - jlmoy
@@ -9,167 +10,159 @@ import java.lang.Math;
 
 public class MatrixProduct {
 
-    //Compute and return the product of A, B matrices using “simple” DAC algorithm presented in class.
-   public static int[][] matrixProduct_DAC (int[][] A, int[][] B) throws IllegalArgumentException {
+    // Compute and return the product of A, B matrices using “simple” DAC algorithm
+    // presented in class.
+    public static int[][] matrixProduct_DAC(int[][] A, int[][] B) throws IllegalArgumentException {
         System.out.println("CHECK: " + validityCheck(A, B));
-      if (!validityCheck(A, B)) throw new IllegalArgumentException();
-      return matrixProductRecurrsive(A, 0, 0, B, 0, 0, A.length);
-   }
+        if (!validityCheck(A, B))
+            throw new IllegalArgumentException();
+        return matrixProductRecurrsive(A, 0, 0, B, 0, 0, A.length);
+    }
 
-   private static int[][] matrixProductRecurrsive(int[][] A, int startRowA, int startColA, int[][] B, int startRowB, int startColB, int n) {
-      int[][] C = new int[n][n];
-      if (n == 1) {
+    private static int[][] matrixProductRecurrsive(int[][] A, int startRowA, int startColA, int[][] B, int startRowB,
+            int startColB, int n) {
+        int[][] C = new int[n][n];
+        if (n == 1) {
             int a = A[startRowA][startColA];
             int b = B[startRowB][startColB];
             C[0][0] = a * b;
-            // System.out.println("SINGLE VAL A: " + a);
-            // System.out.println("SINGLE VAL B: " + b);
-      }
-      else {
-         int midpoint = n/2;
+        } else {
+            int midpoint = n / 2;
 
-        //  System.out.println("A: " + startRowA + " " + startColA);
-        //  TestMatrixProduct.printMatrix(A);
+            System.out.println("MID: " + midpoint);
 
-        //  System.out.println("B: " + startRowB + " " + startColB);
-        //  TestMatrixProduct.printMatrix(B);
+            // C11 = A11 * B11 + A12 * B21
+            int[][] C11 = addMatrices(
+                    matrixProductRecurrsive(A, startRowA, startColA, B, startRowB, startColB, midpoint),
+                    matrixProductRecurrsive(A, startRowA, startColA + midpoint, B, startRowB + midpoint, startColB,
+                            midpoint),
+                    midpoint);
 
-         System.out.println("MID: " + midpoint);
-
-         // C11 = A11 * B11 + A12 * B21
-         int[][] C11 = addMatrices(
-            matrixProductRecurrsive(A, startRowA, startColA, B, startRowB, startColB, midpoint),
-            matrixProductRecurrsive(A, startRowA, startColA + midpoint, B, startRowB + midpoint, startColB, midpoint),
-            midpoint);
-
-         // C12 = A11 * B12 + A12 * B22
-         int[][] C12 = addMatrices(
-            matrixProductRecurrsive(A, startRowA, startColA, B, startRowB, startColB + midpoint, midpoint),
-            matrixProductRecurrsive(A, startRowA, startColA + midpoint, B, startRowB + midpoint, startColB + midpoint, midpoint),
-            midpoint);
+            // C12 = A11 * B12 + A12 * B22
+            int[][] C12 = addMatrices(
+                    matrixProductRecurrsive(A, startRowA, startColA, B, startRowB, startColB + midpoint, midpoint),
+                    matrixProductRecurrsive(A, startRowA, startColA + midpoint, B, startRowB + midpoint,
+                            startColB + midpoint, midpoint),
+                    midpoint);
 
             System.out.println("C12: ");
             TestMatrixProduct.printMatrix(C12);
 
-         // C21 = A21 * B11 + A22 * B21
-         int[][] C21 = addMatrices(
-            matrixProductRecurrsive(A, startRowA + midpoint, startColA, B, startRowB, startColB, midpoint),
-            matrixProductRecurrsive(A, startRowA+ midpoint, startColA + midpoint, B, startRowB + midpoint, startColB, midpoint),
-            midpoint);
+            // C21 = A21 * B11 + A22 * B21
+            int[][] C21 = addMatrices(
+                    matrixProductRecurrsive(A, startRowA + midpoint, startColA, B, startRowB, startColB, midpoint),
+                    matrixProductRecurrsive(A, startRowA + midpoint, startColA + midpoint, B, startRowB + midpoint,
+                            startColB, midpoint),
+                    midpoint);
 
             System.out.println("C21: ");
             TestMatrixProduct.printMatrix(C21);
 
-         // C22 = A21 * B12 + A22 * B22
-         int[][] C22 = addMatrices(
-            matrixProductRecurrsive(A, startRowA + midpoint, startColA, B, startRowB, startColB + midpoint, midpoint),
-            matrixProductRecurrsive(A, startRowA + midpoint, startColA + midpoint, B, startRowB + midpoint, startColB + midpoint, midpoint),
-            midpoint);
+            // C22 = A21 * B12 + A22 * B22
+            int[][] C22 = addMatrices(
+                    matrixProductRecurrsive(A, startRowA + midpoint, startColA, B, startRowB, startColB + midpoint,
+                            midpoint),
+                    matrixProductRecurrsive(A, startRowA + midpoint, startColA + midpoint, B, startRowB + midpoint,
+                            startColB + midpoint, midpoint),
+                    midpoint);
 
             System.out.println("C22: ");
             TestMatrixProduct.printMatrix(C22);
 
-        C = constructMatrixFromQuadrants(C11, C12, C21, C22);
+            C = constructMatrixFromQuadrants(C11, C12, C21, C22);
 
+        }
+
+        return C;
     }
 
-    return C;
-   }
+    public static int[][] constructMatrixFromQuadrants(int[][] Q11, int[][] Q12, int[][] Q21, int[][] Q22) {
+        int quadN = Q11.length;
+        int[][] result = new int[quadN * 2][quadN * 2];
+        for (int i = 0; i < quadN; i++) {
+            for (int j = 0; j < quadN; j++) {
+                result[i][j] = Q11[i][j];
+                result[i + quadN][j] = Q21[i][j];
+                result[i][j + quadN] = Q12[i][j];
+                result[i + quadN][j + quadN] = Q22[i][j];
+            }
+        }
+        return result;
+    }
 
- public static int[][] constructMatrixFromQuadrants(int[][] Q11, int[][] Q12, int[][] Q21, int[][] Q22) {
-     int quadN = Q11.length;
-     int[][] result  = new int[quadN*2][quadN*2];
-     for (int i = 0; i < quadN; i++) {
-         for (int j = 0; j < quadN; j++) {
-             result[i][j] = Q11[i][j];
-             result[i + quadN][j] = Q21[i][j];
-             result[i][j + quadN] = Q12[i][j];
-             result[i + quadN][j + quadN] = Q22[i][j];
-         }
-     }
-     return result;
- }
+    // Compute and return the product of A, B matrixes using Strassen’s algorithm
+    // presented in class.
+    public static int[][] matrixProduct_Strassen(int[][] A, int[][] B) throws IllegalArgumentException {
+        if (!validityCheck(A, B))
+            throw new IllegalArgumentException();
+        int[][] C = strassenAdds(A, B, A.length);
 
+        // strassenP();
+        // strassenC();
+        return C;
+    }
 
-    //Compute and return the product of A, B matrixes using Strassen’s algorithm presented in class.
-   public static int[][] matrixProduct_Strassen(int[][] A, int[][] B) throws IllegalArgumentException {
-      if (!validityCheck(A, B)) throw new IllegalArgumentException();
-      int[][] C = strassenAdds(A, B, A.length);
+    private static int[][] strassenAdds(int[][] A, int[][] B, int n) {
+        int mid = A.length / 2;
+        // s1 = B12 - B22
+        int[][] s1 = addMatrices(B, 0, mid, B, mid, mid, mid);
+        // s2 = A11 + A12
+        int[][] s2 = addMatrices(A, 0, 0, B, 0, mid, mid);
+        // s3 = A21 + A22
+        int[][] s3 = addMatrices(A, mid, 0, A, mid, mid, mid);
+        // S4 = B21 - B11
+        int[][] s4 = subMatrices(B, mid, 0, B, 0, 0, mid);
+        // s5 = A11 + A22
+        int[][] s5 = addMatrices(A, 0, 0, A, mid, mid, mid);
+        // s6 = B11 + B22
+        int[][] s6 = addMatrices(B, 0, 0, B, mid, mid, mid);
+        // s7 = A12 - A22
+        int[][] s7 = subMatrices(A, 0, mid, A, mid, mid, mid);
+        // s8 = B21 + B22
+        int[][] s8 = addMatrices(B, mid, 0, B, mid, mid, mid);
+        // s9 = A11 - A21
+        int[][] s9 = subMatrices(A, 0, 0, A, mid, 0, mid);
+        // s10 = B11 + B12
+        int[][] s10 = addMatrices(B, 0, 0, B, 0, mid, mid);
 
-      // strassenP();
-      // strassenC();
-      return C;
-   }
+        return s1;
+    }
 
-   private static int[][] strassenAdds(int[][] A, int[][] B, int n) {
-      int mid = A.length/2;
-      // s1 = B12 - B22
-      int[][] s1 = addMatrices(B, 0, mid, B, mid, mid, mid);
-      // s2 = A11 + A12
-      int[][] s2 = addMatrices(A, 0, 0, B, 0, mid, mid);
-      // s3 = A21 + A22
-      int[][] s3 = addMatrices(A, mid, 0, A, mid, mid, mid);
-      // S4 = B21 - B11
-      int[][] s4 = subMatrices(B, mid, 0, B, 0, 0, mid);
-      // s5 = A11 + A22
-      int[][] s5 = addMatrices(A, 0, 0, A, mid, mid, mid);
-      // s6 = B11 + B22
-      int[][] s6 = addMatrices(B, 0, 0, B, mid, mid, mid);
-      // s7 = A12 - A22
-      int[][] s7 = subMatrices(A, 0, mid, A, mid, mid, mid);
-      // s8 = B21 + B22
-      int[][] s8 = addMatrices(B, mid, 0, B, mid, mid, mid);
-      // s9 = A11 - A21
-      int[][] s9 = subMatrices(A, 0, 0, A, mid, 0, mid);
-      // s10 = B11 + B12
-      int[][] s10 = addMatrices(B, 0, 0, B, 0, mid, mid);
+    private int[][] strassenP(int[][] A, int[][] B, int n) {
+        int[][] C = new int[n][n];
+        // // p1 = A11 * S1
+        // int[][] p1 = matrixProduct_Strassen();
+        // // p2 = s2 * B22
+        // int[][] p1 = matrixProduct_Strassen();
+        // // p3 = s3 * B11
+        // int[][] p1 = matrixProduct_Strassen();
+        // // p4 = A22 * s4
+        // int[][] p1 = matrixProduct_Strassen();
+        // // p5 = s5 * s6
+        // int[][] p1 = matrixProduct_Strassen();
+        // // p6 = s7 * s8
+        // int[][] p1 = matrixProduct_Strassen();
+        // // p7 = s9 * s10
+        // int[][] p1 = matrixProduct_Strassen();
+        return C;
+    }
 
-      return s1;
-   }
+    private int[][] strassenC(int[][] A, int[][] B, int n) {
+        int[][] C = new int[n][n];
+        // C11 = p5 + p4 - p2 + p6
+        // C12 = p1 + p2
+        // C21 = p3 + p4
+        // C22 = p5 + p1 - p3 - p7
+        return C;
+    }
 
-   private int[][] strassenP(int[][]A, int[][] B, int n) {
-      int[][] C = new int[n][n];
-      // // p1 = A11 * S1
-      // int[][] p1 = matrixProduct_Strassen();
-      // // p2 = s2 * B22
-      // int[][] p1 = matrixProduct_Strassen();
-      // // p3 = s3 * B11
-      // int[][] p1 = matrixProduct_Strassen();
-      // // p4 = A22 * s4
-      // int[][] p1 = matrixProduct_Strassen();
-      // // p5 = s5 * s6
-      // int[][] p1 = matrixProduct_Strassen();
-      // // p6 = s7 * s8
-      // int[][] p1 = matrixProduct_Strassen();
-      // // p7 = s9 * s10
-      // int[][] p1 = matrixProduct_Strassen();
-      return C;
-   }
-
-   private int[][] strassenC(int[][] A, int[][] B, int n) {
-      int[][] C = new int[n][n];
-      // C11 = p5 + p4 - p2 + p6
-      // C12 = p1 + p2
-      // C21 = p3 + p4
-      // C22 = p5 + p1 - p3 - p7
-      return C;
-   }
-
-   private static boolean validityCheck(int[][] A, int[][] B) {
-      // System.out.println(A.length);
-
-      // System.out.println(A.length == B.length);
-      // System.out.println(A[0].length == B[0].length);
-      // System.out.println(A.length == A[0].length);
-      // System.out.println(isSquare(A.length));
-      // System.out.println(A.length % 2 == 0);
-
-      if (A.length == B.length && A[0].length == B[0].length && A.length == A[0].length && isPow2(A.length)) {
-         return true; // is Valid
-      } else {
-         return false; // is inValid
-      }
-   }
+    private static boolean validityCheck(int[][] A, int[][] B) {
+        if (A.length == B.length && A[0].length == B[0].length && A.length == A[0].length && isPow2(A.length)) {
+            return true; // is Valid
+        } else {
+            return false; // is inValid
+        }
+    }
 
     // For adding whole matrices
     public static int[][] addMatrices(int[][] A, int[][] B, int n) {
@@ -177,27 +170,28 @@ public class MatrixProduct {
     }
 
     // For adding 1 quadrant from each of 2 matrices
-   public static int[][] addMatrices(int [][] A, int startRowA, int startColA, int[][] B, int startRowB, int startColB, int n) {
+    public static int[][] addMatrices(int[][] A, int startRowA, int startColA, int[][] B, int startRowB, int startColB,
+            int n) {
         int[][] result = new int[n][n];
         int colA, colB, rowA, rowB;
         colA = startColA;
         colB = startColB;
-        
+
         for (int i = 0; i < n; i++) {
             rowA = startRowA;
             rowB = startRowB;
-                
-                for (int j = 0; j < n; j++) {
-                    result[i][j] = A[colA][rowA] + B[colB][rowB];
-                    rowA++;
-                    rowB++;
-                }
-                colA++;
-                colB++;
-                 
+
+            for (int j = 0; j < n; j++) {
+                result[i][j] = A[colA][rowA] + B[colB][rowB];
+                rowA++;
+                rowB++;
+            }
+            colA++;
+            colB++;
+
         }
         return result;
-   }
+    }
 
     public static int[][] subMatrices(int[][] A, int startRowA, int startColA, int[][] B, int startRowB, int startColB,
             int n) {
@@ -218,10 +212,12 @@ public class MatrixProduct {
     }
 
     public static boolean isPow2(int n) {
-        if (n == 0) return false;
+        if (n == 0)
+            return false;
 
         while (n != 1) {
-            if (n % 2 != 0) return false;
+            if (n % 2 != 0)
+                return false;
             n = n / 2;
         }
 
--- part2/TestMatrixProduct.java
+++ part2/TestMatrixProduct.java
@@ -26,31 +26,21 @@ public class TestMatrixProduct {
 
         int[][] result = MatrixProduct.matrixProduct_DAC(A, B);
 
-        // printMatrix(result);
-
         boolean passed = matrixEquality(result, expected);
         System.out.println("TEST MULT MATRIX SINGLE VAL MATRICES: " + (passed ? "PASSED" : "FAILED"));
     }
 
     public static void testMatrixProduct_DAC_UnitMats() {
-        int[][] A = { 
-            { 1, 0 },
-            { 0, 1 }
-        };
+        int[][] A = { { 1, 0 }, { 0, 1 } };
 
         int[][] result = MatrixProduct.matrixProduct_DAC(A, A);
-
-        // printMatrix(result);
 
         boolean passed = matrixEquality(result, A);
         System.out.println("TEST MULT MATRIX UNIT MATRICES: " + (passed ? "PASSED" : "FAILED"));
     }
 
     public static void testMatrixProduct_DAC_UnitMats2() {
-        int[][] A = { { 1, 0, 0, 0 },
-                      { 0, 1, 0, 0 },
-                      { 0, 0, 1, 0 },
-                      { 0, 0, 0, 1 } };
+        int[][] A = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } };
 
         int[][] result = MatrixProduct.matrixProduct_DAC(A, A);
 
@@ -61,14 +51,11 @@ public class TestMatrixProduct {
     }
 
     public static void testMatrixProduct_DAC() {
-        int[][] A = { { 1, 2 },
-                      { 3, 4 } };
+        int[][] A = { { 1, 2 }, { 3, 4 } };
 
-        int[][] B = { { 5, 6 },
-                      { 7, 8 } };
+        int[][] B = { { 5, 6 }, { 7, 8 } };
 
-        int[][] expected = { { 19, 22 },
-                             { 43, 50 } };
+        int[][] expected = { { 19, 22 }, { 43, 50 } };
 
         int[][] result = MatrixProduct.matrixProduct_DAC(A, B);
 
@@ -79,68 +66,34 @@ public class TestMatrixProduct {
     }
 
     public static void testMatrixProduct_DAC2() {
-        int[][] A = { 
-            { 1, 2, 3, 4 },
-            { 5, 6, 7, 8 },
-            { 9, 10, 11, 12 },
-            { 13, 14, 15, 16 }
-        };
+        int[][] A = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
 
-        int[][] B = {
-            { 16, 15, 14, 13 },
-            { 12, 11, 10, 9 },
-            { 8, 7, 6, 5 },
-            { 4, 3, 2, 1 }
-        };
+        int[][] B = { { 16, 15, 14, 13 }, { 12, 11, 10, 9 }, { 8, 7, 6, 5 }, { 4, 3, 2, 1 } };
 
-        int[][] expected = {
-            { 80, 70, 60, 50 },
-            { 240, 214, 188, 162 },
-            { 400, 358, 316, 274 },
-            { 560, 502, 444, 386 }
-        };
+        int[][] expected = { { 80, 70, 60, 50 }, { 240, 214, 188, 162 }, { 400, 358, 316, 274 },
+                { 560, 502, 444, 386 } };
 
         int[][] result = MatrixProduct.matrixProduct_DAC(A, B);
 
         printMatrix(expected);
         printMatrix(result);
 
-
         boolean passed = matrixEquality(result, expected);
         System.out.println("TEST MULT MATRIX MATRICES: " + (passed ? "PASSED" : "FAILED"));
     }
 
     public static void testConstructMatrixFromQuadrants() {
-        int[][] Q11 = {
-            { 11, 11 },
-            { 11, 11 }
-        };
+        int[][] Q11 = { { 11, 11 }, { 11, 11 } };
 
-        int[][] Q12 = {
-            { 12, 12 },
-            { 12, 12 }
-        };
+        int[][] Q12 = { { 12, 12 }, { 12, 12 } };
 
-        int[][] Q21 = {
-            { 21, 21 },
-            { 21, 21 }
-        };
+        int[][] Q21 = { { 21, 21 }, { 21, 21 } };
 
-        int[][] Q22 = {
-            { 22, 22 },
-            { 22, 22 }
-        };
+        int[][] Q22 = { { 22, 22 }, { 22, 22 } };
 
-        int[][] expected = {
-            { 11, 11, 12, 12 },
-            { 11, 11, 12, 12 },
-            { 21, 21, 22, 22 },
-            { 21, 21, 22, 22 }
-        };
+        int[][] expected = { { 11, 11, 12, 12 }, { 11, 11, 12, 12 }, { 21, 21, 22, 22 }, { 21, 21, 22, 22 } };
 
         int[][] result = MatrixProduct.constructMatrixFromQuadrants(Q11, Q12, Q21, Q22);
-
-        // printMatrix(result);
 
         boolean passed = matrixEquality(result, expected);
         System.out.println("TEST CONSTRUCT MATRIX FROM QUADRANTS: " + (passed ? "PASSED" : "FAILED"));
@@ -148,30 +101,13 @@ public class TestMatrixProduct {
 
     public static void testAddMatrices() {
 
-        int[][] A = {
-            { 1, 2, 3, 4 },
-            { 5, 6, 7, 8 },
-            { 9, 10, 11, 12 },
-            { 13, 14, 15, 16 }
-        };
+        int[][] A = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
 
-        int[][] B = {
-            { 16, 15, 14, 13 },            
-            { 12, 11, 10, 9 },
-            { 8, 7, 6, 5 },
-            { 4, 3, 2, 1 } 
-        };
+        int[][] B = { { 16, 15, 14, 13 }, { 12, 11, 10, 9 }, { 8, 7, 6, 5 }, { 4, 3, 2, 1 } };
 
-        int[][] expected = {
-            { 17, 17, 17, 17 },
-            { 17, 17, 17, 17 },
-            { 17, 17, 17, 17 },
-            { 17, 17, 17, 17 }
-        };
+        int[][] expected = { { 17, 17, 17, 17 }, { 17, 17, 17, 17 }, { 17, 17, 17, 17 }, { 17, 17, 17, 17 } };
 
         int[][] result = MatrixProduct.addMatrices(A, 0, 0, B, 0, 0, A.length);
-
-        // printMatrix(C);
 
         boolean passed = matrixEquality(result, expected);
         System.out.println("TEST ADD MATRIX: " + (passed ? "PASSED" : "FAILED"));
@@ -180,17 +116,13 @@ public class TestMatrixProduct {
 
     public static void testAddMatrices2() {
 
-        int[][] A = { { 1, 1, 2, 2 },
-                      { 1, 1, 2, 2 },
-                      { 3, 3, 4, 4 },
-                      { 3, 3, 4, 4 } };
+        int[][] A = { { 1, 1, 2, 2 }, { 1, 1, 2, 2 }, { 3, 3, 4, 4 }, { 3, 3, 4, 4 } };
 
-        int[][] expected = { { 5, 5 },
-                             { 5, 5} };
+        int[][] expected = { { 5, 5 }, { 5, 5 } };
 
-        int mid = A.length/2;
+        int mid = A.length / 2;
 
-        int[][] result = MatrixProduct.addMatrices(A, 0, 0, A, mid, mid, A.length/2);
+        int[][] result = MatrixProduct.addMatrices(A, 0, 0, A, mid, mid, A.length / 2);
 
         // printMatrix(result);
 
@@ -202,52 +134,43 @@ public class TestMatrixProduct {
     public static void testAddMatricesUnitQuadrant() {
 
         boolean passed = true;
-        int[][] A = {{1, 2},{3, 4}};
-        int[][] B = {{0, 0},{0, 1}};
+        int[][] A = { { 1, 2 }, { 3, 4 } };
+        int[][] B = { { 0, 0 }, { 0, 1 } };
 
-        // printMatrix(A);
-        // printMatrix(B);
-
-        int midpoint = A.length/2;
+        int midpoint = A.length / 2;
         int[][] C = MatrixProduct.addMatrices(A, midpoint, midpoint, B, midpoint, midpoint, midpoint);
 
-        // printMatrix(C);
-
-        if (C.length != 1) passed = false;
-        if (C[0].length != 1) passed = false;
-        if (C[0][0] != 5) passed = false;
+        if (C.length != 1)
+            passed = false;
+        if (C[0].length != 1)
+            passed = false;
+        if (C[0][0] != 5)
+            passed = false;
         System.out.println("TEST ADD MATRIX QUAD SIMPLE 1: " + (passed ? "PASSED" : "FAILED"));
 
         int[][] D = MatrixProduct.addMatrices(A, 0, midpoint, B, 0, midpoint, midpoint);
-        if (D[0][0] != 3) passed = false;
+        if (D[0][0] != 3)
+            passed = false;
         System.out.println("TEST ADD MATRIX QUAD SIMPLE 2: " + (passed ? "PASSED" : "FAILED"));
     }
-
-    // public static int[][] addMatrices(
-        // int [][] A, int startRowA, int startColA,
-        // int[][] B, int startRowB, int startColB, int n)
-
-        
 
     public static void testAddMatricesSimple() {
 
         boolean passed = true;
-        int[][] A = {{1, 2},{3, 4}};
-        int[][] B = {{4, 3},{2, 1}};
-
-        // printMatrix(A);
-        // printMatrix(B);
+        int[][] A = { { 1, 2 }, { 3, 4 } };
+        int[][] B = { { 4, 3 }, { 2, 1 } };
 
         int[][] C = MatrixProduct.addMatrices(A, 0, 0, B, 0, 0, A.length);
 
-        // printMatrix(C);
-
-        if (C.length != 2) passed = false;
-        if (C[0].length != 2) passed = false;
+        if (C.length != 2)
+            passed = false;
+        if (C[0].length != 2)
+            passed = false;
 
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A.length; j++) {
-                if (C[i][j] != 5) passed = false;
+                if (C[i][j] != 5)
+                    passed = false;
             }
         }
 
@@ -255,8 +178,10 @@ public class TestMatrixProduct {
 
         int[][] D = MatrixProduct.addMatrices(A, B, A.length);
 
-        if (D.length != 2) passed = false;
-        if (D[0].length != 2) passed = false;
+        if (D.length != 2)
+            passed = false;
+        if (D[0].length != 2)
+            passed = false;
 
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A.length; j++) {
@@ -275,12 +200,7 @@ public class TestMatrixProduct {
         int[][] A = { { 4, 4 }, { 4, 4 } };
         int[][] B = { { 2, 2 }, { 2, 2 } };
 
-        // printMatrix(A);
-        // printMatrix(B);
-
         int[][] C = MatrixProduct.subMatrices(A, 0, 0, B, 0, 0, A.length);
-
-        // printMatrix(C);
 
         if (C.length != 2)
             passed = false;
@@ -302,7 +222,8 @@ public class TestMatrixProduct {
         boolean areEqual = true;
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
-                if (A[i][j] != B[i][j]) areEqual = false;
+                if (A[i][j] != B[i][j])
+                    areEqual = false;
             }
         }
         return areEqual;
